a7caeff713c0aebadbbdfd73e8b8dfb5903e69f2
Ibrahim-Hassan74 <ih3046376@gmail.com>
2026-02-09 03:30:14 +0200

    feat(common): integrate WhatsApp service for message sending

--- SmartMicrobus.Infrastructure/Data/Migrations/20260208010044_intial.cs
+++ SmartMicrobus.Infrastructure/Data/Migrations/20260208010044_intial.cs
@@ -3,7 +3,7 @@ using Microsoft.EntityFrameworkCore.Migrations;
 
 #nullable disable
 
-namespace SmartMicrobus.Infrastructure.Migrations
+namespace SmartMicrobus.Infrastructure.Data.Migrations
 {
     /// <inheritdoc />
     public partial class intial : Migration
---
+++ SmartMicrobus.Infrastructure/Data/Migrations/20260208170626_AddBaseEntityAndUpdatePrimaryKeys.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SmartMicrobus.Infrastructure.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddBaseEntityAndUpdatePrimaryKeys : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Drivers_AspNetUsers_driverId",
+                table: "Drivers");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Passangers_AspNetUsers_PassengerId",
+                table: "Passangers");
+
+            migrationBuilder.RenameColumn(
+                name: "PassengerId",
+                table: "Passangers",
+                newName: "Id");
+
+            migrationBuilder.RenameColumn(
+                name: "driverId",
+                table: "Drivers",
+                newName: "Id");
+
+            migrationBuilder.AddColumn<string>(
+                name: "RefreshToken",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTimeOffset>(
+                name: "RefreshTokenExpirationDateTime",
+                table: "AspNetUsers",
+                type: "datetimeoffset",
+                nullable: false,
+                defaultValue: new DateTimeOffset(new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, 0, 0, 0, 0)));
+
+            migrationBuilder.CreateTable(
+                name: "Photos",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    ImageName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    UserId = table.Column<Guid>(type: "uniqueidentifier", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Photos", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Photos_AspNetUsers_UserId",
+                        column: x => x.UserId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Photos_UserId",
+                table: "Photos",
+                column: "UserId",
+                unique: true,
+                filter: "[UserId] IS NOT NULL");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Drivers_AspNetUsers_Id",
+                table: "Drivers",
+                column: "Id",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Passangers_AspNetUsers_Id",
+                table: "Passangers",
+                column: "Id",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Drivers_AspNetUsers_Id",
+                table: "Drivers");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Passangers_AspNetUsers_Id",
+                table: "Passangers");
+
+            migrationBuilder.DropTable(
+                name: "Photos");
+
+            migrationBuilder.DropColumn(
+                name: "RefreshToken",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "RefreshTokenExpirationDateTime",
+                table: "AspNetUsers");
+
+            migrationBuilder.RenameColumn(
+                name: "Id",
+                table: "Passangers",
+                newName: "PassengerId");
+
+            migrationBuilder.RenameColumn(
+                name: "Id",
+                table: "Drivers",
+                newName: "driverId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Drivers_AspNetUsers_driverId",
+                table: "Drivers",
+                column: "driverId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Passangers_AspNetUsers_PassengerId",
+                table: "Passangers",
+                column: "PassengerId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
