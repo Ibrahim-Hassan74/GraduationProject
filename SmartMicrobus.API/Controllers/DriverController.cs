88177cdbae78772f6bc45817520ece75f667b23f
ahmed salah <ahmed.salah200555@gmail.com>
2026-02-28 20:50:26 +0200

    feat(driver): add driver controller with required endpoints

---
+++ SmartMicrobus.API/Controllers/DriverController.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using SmartMicrobus.Core.DTO.Common;
+using SmartMicrobus.Core.DTO.Queue;
+using SmartMicrobus.Core.Enums;
+using SmartMicrobus.Core.ServiceContracts;
+using SmartMicrobus.Core.ServiceContracts.Drivers;
+using System.Security.Claims;
+
+namespace SmartMicrobus.API.Controllers
+{
+    public class DriverController(IDriverService driverService, ILogger<DriverController> logger) : CustomControllerBase
+    {
+        [Authorize(Roles = nameof(UserRole.Driver))]
+        [HttpGet("get-current-postion")]
+        public async Task<IActionResult> CurrentPosition()
+        {
+            var driverId = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+
+            var response = await driverService.GetDashboardAsync(Guid.Parse(driverId!));
+            if (!response.Success)
+            {
+                return BadRequest(response.Message);
+            }
+            return Ok(response.Data);
+        }
+
+        [HttpGet("get-driver-queue")]
+
+        public async Task<IActionResult> DriverQueue(Guid driverId)
+        {
+            if (driverId == Guid.Empty)
+            {
+                return BadRequest("Driver ID is required");
+            }
+
+            var response = await driverService.GetMyQueueAsync(driverId);
+
+            if (!response.Success)
+            {
+                return BadRequest(response.Message);
+            }
+
+            return Ok(response.Data);
+        }
+
+        [HttpPost]
+        [Route("start-trip")]
+        public async Task<IActionResult> StartTrip(Guid driverId)
+        {
+            if (driverId == Guid.Empty)
+            {
+                return BadRequest("Driver ID is required");
+            }
+            var response = await driverService.StartTripAsync(driverId);
+            if (!response.Success)
+            {
+                return BadRequest(response.Message);
+            }
+            return Ok(response.Message);
+        }
+
+        [HttpPost]
+        [Route("end-trip")]
+        public async Task<IActionResult> EndTrip(Guid driverId)
+        {
+            if (driverId == Guid.Empty)
+            {
+                return BadRequest("Driver ID is required");
+            }
+            var response = await driverService.EndTripAsync(driverId);
+            if (!response.Success)
+            {
+                return BadRequest(response.Message);
+            }
+            return Ok(response.Message);
+        }
+    }
+}
