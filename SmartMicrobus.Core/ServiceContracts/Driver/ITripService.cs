83ca2f3818097e93a49bcd78078981b73b205749
ahmed salah <ahmed.salah200555@gmail.com>
2026-03-21 11:43:09 +0200

    refactor(Trip): separate the trips logic in a separated class

---
+++ SmartMicrobus.Core/ServiceContracts/Driver/ITripService.cs
@@ -0,0 +1,14 @@
+﻿using SmartMicrobus.Core.DTO.Common;
+using SmartMicrobus.Core.DTO.Driver;
+
+namespace SmartMicrobus.Core.ServiceContracts.Driver
+{
+    public interface ITripService
+    {
+        Task<ApiResponse> StartTripAsync(Guid driverId);
+
+        Task<ApiResponse> EndTripAsync(Guid driverId);
+
+        Task<ApiResponse> GetDriverHistoryAsync(Guid driverId, DriverHistoryRequest request);
+    }
+}
