85aee639ddfb62f447b30868c9301328999c3f44
ahmed salah <ahmed.salah200555@gmail.com>
2026-03-21 11:44:00 +0200

    feat(Trip): add microbus navigation to the trip entity

--- SmartMicrobus.Core/Domain/Entities/Trip.cs
+++ SmartMicrobus.Core/Domain/Entities/Trip.cs
@@ -10,6 +10,9 @@ namespace SmartMicrobus.Core.Domain.Entities
         public Guid MicrobusId { get; set; }
         public Microbus Microbus { get; set; } = null!;
 
+        public Guid StationId { get; set; }
+        public Station Station { get; set; } = null!;
+        
         public Guid RouteId { get; set; }
         public Route Route { get; set; } = null!;
 
