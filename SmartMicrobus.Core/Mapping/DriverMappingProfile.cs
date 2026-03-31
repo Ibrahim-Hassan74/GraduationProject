6803487355f0e7a42d98640a9ac1f8d75f9a3ad9
amira ashour <93158924as@gmail.com>
2026-03-31 06:07:29 +0200

    feat(Driver): Add GetDriverByPlateNumber endpoint in DriverController and  GetDriverByPlateNumber  in DriverService and repository

--- SmartMicrobus.Core/Mapping/DriverMappingProfile.cs
+++ SmartMicrobus.Core/Mapping/DriverMappingProfile.cs
@@ -98,6 +98,31 @@ namespace SmartMicrobus.Core.Mapping
                 .ForMember(dest => dest.EstimatedArrivalMinutes,
                     opt => opt.MapFrom(src =>
                         (int)Math.Ceiling(src.DistanceKm * 1.2))); // will change it later
+
+
+            CreateMap<Driver, DriverResponse>()
+                .ForMember(dest => dest.From,
+                    opt => opt.MapFrom(src =>
+                        CultureInfo.CurrentUICulture.TwoLetterISOLanguageName == "ar"
+                            ? src.Microbus.Route.FromAr : src.Microbus.Route.FromEn))
+                .ForMember(dest => dest.To,
+                    opt => opt.MapFrom(src =>
+                        CultureInfo.CurrentUICulture.TwoLetterISOLanguageName == "ar"
+                            ? src.Microbus.Route.ToAr : src.Microbus.Route.ToEn))
+                .ForMember(dest => dest.DriverName,
+                   opt => opt.MapFrom(src => src.ApplicationUser.DisplayName))
+               .ForMember(dest => dest.DriverId,
+                   opt => opt.MapFrom(src => src.ApplicationUser.Id))
+               .ForMember(dest => dest.PlateNumber,
+                   opt => opt.MapFrom(src => src.Microbus.PlateNumber))
+               .ForMember(dest => dest.PassengerCount,
+                   opt => opt.MapFrom(src => src.Microbus.PassengerCount))
+               .ForMember(dest => dest.Model,
+                   opt => opt.MapFrom(src => src.Microbus.Model))
+               .ForMember(dest => dest.Color,
+                   opt => opt.MapFrom(src => src.Microbus.Color));
+
+
         }
     }
 }
--- SmartMicrobus.Infrastructure/Repository/DriverRepository.cs
+++ SmartMicrobus.Infrastructure/Repository/DriverRepository.cs
@@ -1,4 +1,5 @@
-﻿using SmartMicrobus.Core.Domain.Entities;
+﻿using Microsoft.EntityFrameworkCore;
+using SmartMicrobus.Core.Domain.Entities;
 using SmartMicrobus.Core.RepositoryContracts;
 using SmartMicrobus.Infrastructure.Data;
 
@@ -6,5 +7,13 @@ namespace SmartMicrobus.Infrastructure.Repository
 {
     public class DriverRepository(ApplicationDbContext context) : GenericRepository<Driver>(context) ,IDriverRepository
     {
+        public async Task<Driver?>GetDriverByPlateNumber(string plateNumber)
+        {
+            var driver = await context.Drivers
+                .Include(d => d.ApplicationUser)
+                .Include(d => d.Microbus).ThenInclude(r=>r.Route)
+                .FirstOrDefaultAsync(d => d.Microbus.PlateNumber == plateNumber);
+            return driver;
+        }
     }
 }
