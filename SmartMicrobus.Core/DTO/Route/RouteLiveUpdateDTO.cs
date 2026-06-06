1d237fd7b1341e44fcbbf2718016dbd4b6af6453
amira ashour <93158924as@gmail.com>
2026-06-06 03:56:31 +0300

    feat(route): add SignalR support for live route updates
    - Add RouteTrackingHub
    - Add RouteLiveUpdateDTO
    - Add route tracking notification contract

--- SmartMicrobus.Core/DTO/Route/RouteLiveUpdateDTO.cs
+++ SmartMicrobus.Core/DTO/Route/RouteLiveUpdateDTO.cs
@@ -1,11 +1,11 @@
 namespace SmartMicrobus.Core.DTO.Route
 {
-    public class RouteSummaryResponse
+    public class RouteLiveUpdateDTO
     {
-        public decimal Price { get; set; }
-        public double DistanceKm { get; set; }
         public int NumberOfMicrobusesInQueue { get; set; }
+
         public int NumberOfMicrobusesOnTheWay { get; set; }
+
         public int? NearestArrivalMinutes { get; set; }
     }
 }
